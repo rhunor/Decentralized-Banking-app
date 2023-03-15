80d3041e8195dd34e0cc9b39579b9f23edfd3d57
rhunor <89919790+rhunor@users.noreply.github.com>
2023-03-15 05:29:55 +0100

    Add files via upload

---
+++ src/dbank_assets/src/index.js
@@ -0,0 +1,43 @@
+import {dbank} from "../../declarations/dbank";
+
+
+window.addEventListener("load", async function () {
+  //console.log("finished loading");
+
+  update()
+
+});
+
+document.querySelector("form").addEventListener("submit",async function (event) {
+
+
+  event.preventDefault();
+
+
+const button = event.target.querySelector("#submit-btn")
+
+const inputAmount = parseFloat(document.getElementById("input-amount").value);
+const outputAmount = parseFloat(document.getElementById("withdrawal-amount").value);
+
+button.setAttribute("disabled", true);
+if (document.getElementById("input-amount").value.length !== 0){
+  await dbank.topUp(inputAmount);
+}
+ if (document.getElementById("withdrawal-amount").value.length !== 0){
+  await dbank.withdraw(outputAmount);
+}
+await dbank.compound();
+
+update()
+
+
+  document.getElementById("input-amount").value="";
+  document.getElementById("withdrawal-amount").value="";
+  button.removeAttribute("disabled");
+  //console.log("submitted");
+});
+
+async function update() {
+  const currentAmount = await dbank.checkbalance();
+  document.getElementById("value").innerText = parseFloat(currentAmount).toFixed(2);
+}
